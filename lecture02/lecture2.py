04d992ea97ac56972ace8c336b0c6f5142518818
Wwqq121 <71969943+Wwqq121@users.noreply.github.com>
2020-10-12 10:57:35 +0300

    Add files via upload

---
+++ lecture02/lecture2.py
@@ -0,0 +1,203 @@
+number = input("Введите предполагаемое число 0-99:")
+if number == str(0):
+	print("Ноль")
+elif number == str(1):
+	print("Один")
+elif number == str(2):
+	print("Два")
+elif number == str(3):
+	print("Три")
+elif number == str(4):
+	print("Четыре")
+elif number == str(5):
+	print("Пять")
+elif number == str(6):
+	print("Шесть")
+elif number == str(7):
+	print("Семь")
+elif number == str(8):
+	print("Восемь")
+elif number == str(9):
+	print("Девять")
+elif number == str(10):
+	print("Десять")
+elif number == str(11):
+	print("Одиннадцать")
+elif number == str(12):
+	print("Двенадцать")
+elif number == str(13):
+	print("Тринадцать")
+elif number == str(14):
+	print("Четырнадцать")
+elif number == str(15):
+	print("Пятнадцать")
+elif number == str(16):
+	print("Шестнадцать")
+elif number == str(17):
+	print("Семнадцать")
+elif number == str(18):
+	print("Восемнадцать")
+elif number == str(19):
+	print("Деватнадцать")
+elif number == str(20):
+	print("Двадцать")
+elif number == str(21):
+	print("Двадцать один")
+elif number == str(22):
+	print("Двадцать два")
+elif number == str(23):
+	print("Двадцать три")
+elif number == str(24):
+	print("Двадцать четыре")
+elif number == str(25):
+	print("Двадцать пять")
+elif number == str(26):
+	print("Двадцать шесть")
+elif number == str(27):
+	print("Двадцать семь")
+elif number == str(28):
+	print("Двадцать восемь")
+elif number == str(29):
+	print("Двадцать девять")
+elif number == str(30):
+	print("Тридцать")
+elif number == str(31):
+	print("Тридцать один")
+elif number == str(32):
+	print("Тридцать два")
+elif number == str(33):
+	print("Тридцать три")
+elif number == str(34):
+	print("Тридцать четыре")
+elif number == str(35):
+	print("Тридцать пять")
+elif number == str(36):
+	print("Тридцать шесть")
+elif number == str(37):
+	print("Тридцать семь")
+elif number == str(38):
+	print("Тридцать восемь")
+elif number == str(39):
+	print("Тридцать девять")
+elif number == str(40):
+	print("Сорок")
+elif number == str(41):
+	print("Сорок один")
+elif number == str(42):
+	print("Сорок два")
+elif number == str(43):
+	print("Сорок три")
+elif number == str(44):
+	print("Сорок четыре")
+elif number == str(45):
+	print("Сорок пять")
+elif number == str(46):
+	print("Сорок шесть")
+elif number == str(47):
+	print("Сорок семь")
+elif number == str(48):
+	print("Сорок восемь")
+elif number == str(49):
+	print("Сорок девять")
+elif number == str(50):
+	print("Пятьдесят")
+elif number == str(51):
+	print("Пятьдесят один")
+elif number == str(52):
+	print("Пятьдесят два")
+elif number == str(53):
+	print("Пятьдесят три")
+elif number == str(54):
+	print("Пятьдесят четыре")
+elif number == str(55):
+	print("Пятьдесят пять")
+elif number == str(56):
+	print("Пятьдесят шесть")
+elif number == str(57):
+	print("Пятьдесят семь")
+elif number == str(58):
+	print("Пятьдесят восемь")
+elif number == str(59):
+	print("Пятьдесят девять")
+elif number == str(60):
+	print("Шестьдесят")
+elif number == str(61):
+	print("Шестьдесят один")
+elif number == str(62):
+	print("Шестьдесят два")
+elif number == str(63):
+	print("Шестьдесят три")
+elif number == str(64):
+	print("Шестьдесят четыре")
+elif number == str(65):
+	print("Шестьдесят пять")
+elif number == str(66):
+	print("Шестьдесят шесть")
+elif number == str(67):
+	print("Шестьдесят семь")
+elif number == str(68):
+	print("Шестьдесят восемь")
+elif number == str(69):
+	print("Шестьдесят девять")
+elif number == str(70):
+	print("Семьдесят")
+elif number == str(71):
+	print("Семьдесят один")
+elif number == str(72):
+	print("Семьдесят два")
+elif number == str(73):
+	print("Семьдесят три")
+elif number == str(74):
+	print("Семьдесят четыре")
+elif number == str(75):
+	print("Семьдесят пять")
+elif number == str(76):
+	print("Семьдесят шесть")
+elif number == str(77):
+	print("Семьдесят семь")
+elif number == str(78):
+	print("Семьдесят восемь")
+elif number == str(79):
+	print("Семьдесят девять")
+elif number == str(80):
+	print("Восемьдесят")
+elif number == str(81):
+	print("Восемьдесят один")
+elif number == str(82):
+	print("Восемьдесят два")
+elif number == str(83):
+	print("Восемьдесят три")
+elif number == str(84):
+	print("Восемьдесят четыре")
+elif number == str(85):
+	print("Восемьдесят пять")
+elif number == str(86):
+	print("Восемьдесят шесть")
+elif number == str(87):
+	print("Восемьдесят семь")
+elif number == str(88):
+	print("Восемьдесят восемь")
+elif number == str(89):
+	print("Восемьдесят девять")
+elif number == str(90):
+	print("Девяносто")
+elif number == str(91):
+	print("Девяносто один")
+elif number == str(92):
+	print("Девяносто два")
+elif number == str(93):
+	print("Девяносто три")
+elif number == str(94):
+	print("Девяносто четыре")
+elif number == str(95):
+	print("Девяносто пять")
+elif number == str(96):
+	print("Девяносто шесть")
+elif number == str(97):
+	print("Девяносто семь")
+elif number == str(98):
+	print("Девяносто восемь")
+elif number == str(99):
+	print("Девяносто девять")
+else:
+	print("Error")
